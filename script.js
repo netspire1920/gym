c3dd870673c8158dc4534f193ef5ae3c38870f12
Jay Bhatade <138126082+netspire1920@users.noreply.github.com>
2024-05-01 20:46:26 +0530

    update

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
 
-
-// // Add a click event listener to the hamburger menu button
-// document.querySelector('.navbar-toggler').addEventListener('click', function() {
-//     // Toggle the "collapsed" class on the hamburger menu button
-//     this.classList.toggle('collapsed');
-  
-//     // Toggle the "show" class on the navbar collapse element
-//     document.querySelector('.navbar-collapse').classList.toggle('show');
-  
-//     // Toggle the "not-collapsed" class on the hamburger menu button
-//     this.classList.toggle('not-collapsed');
-//   });
-
-
 const scroll = new LocomotiveScroll({
     el: document.querySelector('[data-scroll-container]'),
     smooth: true
 });
+
+document.querySelector("#h123").addEventListener("click", () =>{
+    scroll.scrollTo(document.querySelector("#page1"));
+})
+
+
+document.querySelector("#a123").addEventListener("click", () =>{
+    scroll.scrollTo(document.querySelector("#page2"));
+})
+
+
+document.querySelector("#s123").addEventListener("click", () =>{
+    scroll.scrollTo(document.querySelector("#page3"));
+})
+
+
+document.querySelector("#m123").addEventListener("click", () =>{
+    scroll.scrollTo(document.querySelector("#page4"));
+})
+
