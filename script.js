1997999628b9d8eb7db345a8ba1ae831f13213e6
Jay Bhatade <138126082+netspire1920@users.noreply.github.com>
2024-05-03 17:19:11 +0530

    update

--- script.js
+++ script.js
@@ -20,6 +20,39 @@ document.querySelector("#s123").addEventListener("click", () =>{
 
 
 document.querySelector("#m123").addEventListener("click", () =>{
+    scroll.scrollTo(document.querySelector("#page4"));
+})
+
+document.querySelector("#c123").addEventListener("click", () =>{
+    scroll.scrollTo(document.querySelector("#contact"));
+})
+
+
+document.querySelector("#footer").addEventListener("click", () =>{
+    scroll.scrollTo(document.querySelector("#page1"));
+})
+
+
+document.querySelector("#h1234").addEventListener("click", () =>{
+    scroll.scrollTo(document.querySelector("#page1"));
+})
+
+
+document.querySelector("#a1234").addEventListener("click", () =>{
+    scroll.scrollTo(document.querySelector("#page2"));
+})
+
+
+document.querySelector("#s1234").addEventListener("click", () =>{
+    scroll.scrollTo(document.querySelector("#page3"));
+})
+
+
+document.querySelector("#m1234").addEventListener("click", () =>{
+    scroll.scrollTo(document.querySelector("#page4"));
+})
+
+document.querySelector("#c1234").addEventListener("click", () =>{
     scroll.scrollTo(document.querySelector("#contact"));
 })
 
